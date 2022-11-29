b0ca7ef6445d7fed6ede86d6ee9c449d035ad1db
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2022-11-29 16:22:59 +0100

    Finished the work of the palindrome word

---
+++ js/palindrome.js
@@ -0,0 +1,32 @@
+const textInputElement = document.querySelector('#palindrome-word-analyser input');
+const formElement = document.querySelector('form');
+const formBtnElement = document.querySelector('form .my-btn');
+
+
+formBtnElement.addEventListener('click', function(){
+    let userWord = textInputElement.value;
+    const pElement = document.createElement('p');
+    
+
+    pElement.innerHTML = isPalindrome(userWord);
+
+    formElement.after(pElement);
+})
+
+
+
+function isPalindrome(word){
+    word = word.replace(/\s/g,'');
+    let reversedWord = '';
+
+
+    for(let i=word.length-1 ; i>=0 ; i--){
+        reversedWord += word[i];    
+    }
+
+    
+    let finalComment = (word == reversedWord) ? 'La parola è palindroma' : 'La parola non è palindroma';
+
+    console.log(reversedWord)
+    return finalComment;
+}
