ac53806da392c3cf86220ab61d60c227f3bbc2ef
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2022-11-29 18:04:54 +0100

    Corrected an error

--- js/odd&Even.js
+++ js/odd&Even.js
@@ -15,7 +15,7 @@ oddVsEvenSectionFormBtn.addEventListener('click' , function(){
 
 
     if(controlResult){
-        userNumber = parseFloat(userNumber , '10');
+        userNumber = parseFloat(userNumber , 10);
 
         let pcNumber = getARandomNumber(1 , 5);
         let sum = getSumOfNumbers(userNumber,pcNumber);
@@ -40,6 +40,7 @@ function whoIsChecked(inputRadio1,inputRadio2){
            alert('Non hai ancora scelto se pari o dispari');
 }
 
+
 function isValid(value){
     value = value.replace(/\s/g,'');
 
@@ -49,19 +50,22 @@ function isValid(value){
         return alert('ATTENZIONE!\nPuoi digitare solo valori numerici');
     }
 
-    value = parseFloat(value,'10');
+    value = parseFloat(value,10);
 
     return ((value - Math.floor(value)) !== 0) ? alert('ATTENZIONE!\nNon puoi inserire numeri decimali') : 
         (value<1 || value>5) ? alert('Non hai inserito un valore valido') : true
 }
 
+
 function getARandomNumber(minValue , maxValue){
     return Math.floor(Math.random() * (maxValue - minValue + 1) + minValue);
 }
 
+
 function getSumOfNumbers(num1 , num2){
     return num1+num2;
 }
+
 
 function isOddOrEven(num){
     return (num%2 === 0) ? 'odd' : 'even';
